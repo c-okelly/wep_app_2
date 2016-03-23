e628722c2ec36e774bab19a8de167af791459aad
c-okelly <okellyconor@gmail.com>
2016-03-23 17:07:19 +0000

    Basic funcionality on form working

--- Site/wheather.js
+++ Site/wheather.js
@@ -1,20 +1,34 @@
 $(document).ready(function() {
-//    window.alert("Hello!");
+    window.alert("Hello!");
     var json_file;
 //    json = call_api();
-    json_file = call_3();
-    console.log(json_file);
-    $(".test").append(json_file.city.name);
+//    json_file = call_3();
+//    console.log(json_file);
+//    $(".test").append(json_file.city.name);
     
 });
 
-var call_api = function() {var json_file =
-$.getJSON("http://api.openweathermap.org/data/2.5/forecast/daily?lat=53.3498053&lon=-6.260309699999993&mode=json&appid=3a63ff88497e73a0dd39208e8e969b5e&units=metric&cnt=5", function(json) {
-//    console.log(json);
-    console.log(json.city.id);
-});
-return json_file;
+
+
+var call_api_load_map = function() {
+    var search_city = $("input[name=location]").val();
+    var country_code = $("#country_code option:selected").val();
+    var units = $("input[name=units]:checked").val();
+    var no_days = $("input[name=no_days]:checked").val();    
+    var url_start = "http://api.openweathermap.org/data/2.5/forecast?q="
+    var api_key = "3a63ff88497e73a0dd39208e8e969b5e";
+    
+    var search_url = url_start + search_city + "," + country_code + "&mode=json&appid=" + api_key + "&units=" + units
+    
+    "http://api.openweathermap.org/data/2.5/forecast/daily?q= Dublin,IE &mode=json&appid= 3a63ff88497e73a0dd39208e8e969b5e &units= metric &cnt=5%22"
+    
+    alert(search_url);
+    
 };
+
+var print = function() {
+    alert("hello");
+}
 
 
 var call_2 = function () {
